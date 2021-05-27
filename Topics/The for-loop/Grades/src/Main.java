c3d14d0793235fd253e269ec2b99adb3425466e5
miannuzzi <marcelo.iannuzzi@wdc.com>
2021-05-27 08:55:43 -0700

    Simple Bot push

---
+++ Topics/The for-loop/Grades/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int students = scanner.nextInt();
+        int grade = 0;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+
+
+        for (int i = 0; i<students; i++){
+            grade = scanner.nextInt();
+            if(grade == 5){
+                a++;
+            }else if(grade == 4){
+                b++;
+            }else if(grade == 3){
+                c++;
+            }else if(grade == 2){
+                d++;
+            }
+        }
+
+        System.out.println(d + " " + c + " " + b + " " + a);
+
+    }
+}
